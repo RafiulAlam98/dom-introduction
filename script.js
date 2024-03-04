ec2ba09e9fbcaa45aef1c601abe3c345ef831744
RafiulAlam98 <mrafiul.alam7@gmail.com>
2024-03-04 22:58:01 +0600

    added

---
+++ script.js
@@ -0,0 +1,11 @@
+
+    const makeColorGreen = document.getElementById('make-color-green')
+
+    function handleBgColor(){
+        document.body.style.backgroundColor = 'green'
+    }
+
+    makeColorGreen.addEventListener('click', function (){
+        document.body.style.backgroundColor = 'green'
+    }
+)
